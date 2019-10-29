52a950f1ba6e8cb21fac35b3c20ed3fcb39176b4
James Perez <danjame@hotmail.com>
2019-10-29 12:30:40 -0500

    parametros del sistema

--- parameters.m
+++ parameters.m
@@ -1,18 +1,18 @@
-%% Definicion de parï¿½metros
+%% Definicion de parámetros
 
 potenciaNominal = 250;                                                     % Potencia nominal del panel [W]
-areaPanel = 1.6;                                                           % ï¿½rea del panel [m^2]
+areaPanel = 1.6;                                                           % Área del panel [m^2]
 eficienciaPanel = potenciaNominal/(areaPanel*1000);                        % Eficiencia del panel
-b = -0.0043;                                                               % Coeficiente tï¿½rmico [1/K]
+b = -0.0043;                                                               % Coeficiente térmico [1/K]
 a = 0.9;                                                                   % Coeficiente de transmitancia
-capacidadTermica = 10140;                                                  % Capacidad tï¿½rmica [J/m^2 - K]
+capacidadTermica = 10140;                                                  % Capacidad térmica [J/m^2 - K]
 sigma = 5.67e-8;                                                           % Coeficiente Stefan-Boltzmann [W/m^2 - K^4]
 emitanciaFrontal = 0.9;                                                    % Emitancia frontal
 emitanciaPosterior = 0.85;                                                 % Emitancia posterior
-anguloInclinacion = 10*pi/180;                                             % ï¿½ngulo de inclinaciï¿½n [rad]
-irradianciaMaxima = 1278;                                                  % Irradiancia mï¿½xima [W/m^2]
+anguloInclinacion = 10*pi/180;                                             % Ángulo de inclinación [rad]
+irradianciaMaxima = 1278;                                                  % Irradiancia máxima [W/m^2]
 
-%% Factores de participaciï¿½n de la transferencia de calor por radiaciï¿½n
+%% Factores de participación de la transferencia de calor por radiación
 
 Ff1 = (1/2) * (1 + cos(pi - 10 * (pi/180)));
 Ff2 = (1/2) * (1 - cos(pi - 10 * (pi/180)));
@@ -20,13 +20,13 @@ Ff2 = (1/2) * (1 - cos(pi - 10 * (pi/180)));
 Fp1 = (1/2) * (1 + cos(10 * (pi/180)));
 Fp2 = (1/2) * (1 - cos(10 * (pi/180)));
 
-%% Parï¿½metros del sistema de riego
+%% Parámetros del sistema de riego
 
-resistenciaRiego = 0.00285;                                                % Resistencia de riego [W/ï¿½C]
+resistenciaRiego = 0.00285;                                                % Resistencia de riego [W/°C]
 emitanciaAgua = 0.9;                                                       % Emitancia del agua
-resistenciaMezclado = resistenciaRiego/5;                                  % Resistencia de mezclado [W/ï¿½C]
+resistenciaMezclado = resistenciaRiego/5;                                  % Resistencia de mezclado [W/°C]
 volumenAguaRiego = 4.5;                                                    % Volumen de agua de riego [l/m]
-capacidadEspecificaAgua = 4180;                                            % Capacidad especï¿½fica [J/kg - K]
-capacidadTermica = volumenAguaRiego * capacidadEspecificaAgua;             % Capacidad tï¿½rmica [J/K]
-coeficienteEnfriamentoCava = 3.5;                                          % Coeficiente enfriamiento dentro de la cava [ï¿½C/W]
-coeficienteResistenciaEnfriamiento = 1/coeficienteEnfriamentoCava;         % Resistencia de enfriamiento [W/ï¿½C]
+capacidadEspecificaAgua = 4180;                                            % Capacidad específica [J/kg - K]
+capacidadTermica = volumenAguaRiego * capacidadEspecificaAgua;             % Capacidad térmica [J/K]
+coeficienteEnfriamentoCava = 3.5;                                          % Coeficiente enfriamiento dentro de la cava [°C/W]
+coeficienteResistenciaEnfriamiento = 1/coeficienteEnfriamentoCava;         % Resistencia de enfriamiento [W/°C]
