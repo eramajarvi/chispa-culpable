40212456f3e0dc2866725bed9b9c731cf129cbcf
James Perez <danjame@hotmail.com>
2019-10-29 12:16:47 -0500

    Script programacion dinamica
    Inicio de creacion del script principal para la programacion dinamica

--- dynamicProgramming_Main.m
+++ dynamicProgramming_Main.m
@@ -4,7 +4,9 @@
 clc; clear all; close all;
 
 %% Cargar datos
+
 % load ('nombredato.mat')
+
 parametros                                      % Cargar parametros
 deltaT = 60;                                    % Delta de tiempo
 N = 1441;                                       % Numero de pasos 24*60 + 1
