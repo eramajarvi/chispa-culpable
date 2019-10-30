209d37536a03c198d732c9706537e20079abba36
James Perez <danjame@hotmail.com>
2019-10-30 03:00:36 -0500

    corregido y terminado script para calcular los perfiles de irrigacion

--- Fk.m
+++ Fk.m
@@ -1,4 +1,4 @@
-function [fk] = Fk(temperaturaAmbiente, variacionTemperaturaPanel_NoIrrigation, variacionTemperaturaPanel_Irrigation, deltaT, temperaturaPanel, vPotenciaIrrigacion)
+function [fk] = Fk(temperaturaAmbiente, variacionTemperaturaPanel_NoIrrigation, vPotenciaIrrigacion, variacionTemperaturaPanel_Irrigation, deltaT, temperaturaPanel)
     
     fk = max(temperaturaAmbiente, (variacionTemperaturaPanel_NoIrrigation * (vPotenciaIrrigacion == 0) + variacionTemperaturaPanel_Irrigation * (vPotenciaIrrigacion == 15)) * deltaT + temperaturaPanel);
 
