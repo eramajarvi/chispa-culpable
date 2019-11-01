2b2c7285d5d346d016d424f9f8a1dc9a0ac391e9
James Perez <danjame@hotmail.com>
2019-11-01 00:09:12 -0500

    chispa culpable main

---
+++ chispaCulpable_Main.m
@@ -0,0 +1,44 @@
+%%% FUNCIÓN PRINCIPAL PROGRAMACIÓN DINÁMICA
+% Modificada para llamar a una función de forma iterativa y guardar los
+% perfiles generados
+
+clc; clear all; close all;
+
+%% Cargar datos
+% Importa la matriz con los datos de todos los días
+load ('C:\Users\james\Documents\ShallowBayDEV\tangente-penitente\MatricesDelArca.mat')
+
+%%
+
+for i = 1 : 1 : size(Irradiancia_Matriz, 1)
+    
+    %% Invocar la programación dinámica
+    TemperaturaAmbiente_DiaActual = TemperaturaAmbiente_Matriz(i, :);
+    Irradiancia_DiaActual = Irradiancia_Matriz(i, :);
+    VelocidadViento_DiaActual = VelocidadViento_Matriz(i, :);
+
+    [irradiancia, temperaturaAmbiente, velocidadViento,...
+        control, temperaturaPanel, temperaturaPanel_NoIrrigation, Pben, ...
+        Pgen_NoIrrigation, energiaBeneficio, energiaNoBeneficio, razon_Irrigacion, ...
+        razon_NoIrrigation, tiempo] = dynamicProgamming_MainFunction(...
+        TemperaturaAmbiente_DiaActual, Irradiancia_DiaActual, VelocidadViento_DiaActual, i);
+
+    %% Graficar datos
+    graphProfiles(irradiancia, temperaturaAmbiente, velocidadViento, control, ...
+        temperaturaPanel, temperaturaPanel_NoIrrigation, Pben, Pgen_NoIrrigation, ...
+        energiaBeneficio, energiaNoBeneficio, i, tiempo);
+
+    numeroDia = i;
+
+    nombreArchivo = ['perfil_Dia', num2str(i)];
+
+    save(nombreArchivo, 'irradiancia', 'temperaturaAmbiente', 'velocidadViento',...
+        'control', 'temperaturaPanel', 'temperaturaPanel_NoIrrigation'...
+        , 'Pben', 'Pgen_NoIrrigation', 'energiaBeneficio', 'energiaNoBeneficio'...
+        , 'razon_Irrigacion', 'razon_NoIrrigation', 'tiempo');
+    
+    clear irradiancia temperaturaAmbiente velocidadViento control ...
+        ejeTiempo ejeTiempo_b temperaturaPanel temperaturaPanel_NoIrrigation ...
+        Pben Pgen_NoIrrigation energiaBeneficio energiaNoBeneficio razon_Irrigacion ...
+        razon_NoIrrigation tiempo;
+end
