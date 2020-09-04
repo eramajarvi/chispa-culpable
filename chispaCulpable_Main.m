f0e2fce9d6dcc270857155dd4030ec7d00d78e5a
James Perez <danjame@hotmail.com>
2020-09-03 21:46:38 -0500

    finalizada rama para generar graficas finales

--- chispaCulpable_Main.m
+++ chispaCulpable_Main.m
@@ -1,27 +1,36 @@
-%% FUNCION PRINCIPAL PROGRAMACION DINAMICA
+% CHISPA CULPABLE
+% Archivo de ejecucion principal
+% Debe ejecutarse este archivo para generar las graficas. Los datos deben
+% cargarse respectivamente desde las rutas establecidas
 
 tic
 clc; clear all; close all;
 
-%% Cargar datos
+% -------------------------------------------------------------------------
+% Importar datos
 % Importa la matriz con los datos de todos los dias
 rutaCargaPD = 'C:\Users\James\Desktop\perfiles PD\';
 rutaCargaTP = 'C:\Users\James\Documents\Github\tangente-penitente\perfilesSBR\Prueba 5\';
 
-for i = 93 : 1 : 93
+for i = 286 : 1 : 286
 
     load([rutaCargaPD, 'perfil_Dia', num2str(i)]);
     load([rutaCargaTP, 'perfil_Dia', num2str(i)]);
     
     t = tiempo/60/60 + 6;
     N = 73;
+    
     tiempo10min12h = 0:600:43200;
     ejeTiempo_b = (linspace(0, tiempo10min12h(end), N-1))/60/60 + 6;
-    
-    %% Graficar datos
-    graphVariablesBasicas(t, temperaturaAmbiente, t, irradiancia, velocidadViento)
+
+% -------------------------------------------------------------------------
+% Generar las graficas
+% Llamada a las funciones que generan las graficas
+
+    graphVariablesBasicas(t, temperaturaAmbiente, irradiancia, velocidadViento)
     graphEnergia(ejeTiempo_b, [energiaBeneficioSBR; energiaBeneficio; energiaNoBeneficio]);
     graphPotencias(ejeTiempo_b, [Pgen_NoIrrigation; PbenSBR; Pben]);
-    graphPotenciaIrrigacion(ejeTiempo_b, controlSBR)
+    graphPotenciaIrrigacion(ejeTiempo_b, controlSBR, ejeTiempo_b, control)
+    
 end
 toc
